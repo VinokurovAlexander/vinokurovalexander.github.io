55c62fd707d2ec7f96d4597cf7721fd886b50017
VinokurovAlexander <46749400+VinokurovAlexander@users.noreply.github.com>
2019-10-25 15:13:52 +0300

    Add files via upload

---
+++ Snow_Alp/js/mask-min.js
@@ -0,0 +1,1 @@
+var telephoneForm=document.querySelector("#telephone"),telephoneCallback=document.querySelector("#modal-telephone"),im=new Inputmask("+7-(999)-999-99-99");im.mask(telephoneForm),im.mask(telephoneCallback);
