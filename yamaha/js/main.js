3a9ad57c8e2812f8609f9aba9cfda9abfec76fd6
Alexander <vinokurov.ag@mail.ru>
2019-11-06 11:14:23 +0300

    Удалил проект yamaha

--- yamaha/js/main.js
+++
@@ -1,7 +0,0 @@
-const view360Btn = document.querySelector('.moto-360-view__btn');
-const view360Block = document.querySelector('.cloudimage-360');
-
-view360Btn.addEventListener('click', () => {
-  view360Block.setAttribute('data-autoplay', 'true');
-  console.log(view360Block);
-})
